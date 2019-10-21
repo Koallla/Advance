99f2439776574bd6d380d4c2bdc2d9237b382229
Koallla <zmiyov2422@gmail.com>
2019-10-21 21:31:19 +0300

    Module-12/Lesson-01

---
+++ Module-12/Lesson-01/js/index.js
@@ -0,0 +1,18 @@
+'use script';
+
+(async function() {
+  try {
+    const result = await fetch('https://api.github.com/users/octocat');
+
+    if (result.status === 403) throw new Error(result.statusText);
+    const data = await result.json();
+
+    const image = document.createElement('img');
+    image.src = data.avatar_url;
+
+    document.body.appendChild(image);
+  } catch (error) {
+    console.log(111111111);
+    console.dir(error);
+  }
+})();
