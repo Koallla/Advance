a8a719928fe5e42e7eca232167cab143045bd32e
Koallla <zmiyov2422@gmail.com>
2019-10-23 21:34:08 +0300

    Module-12/Lesson-02

--- Module-12/Lesson-01/js/index.js
+++ Module-12/Lesson-01/js/index.js
@@ -1,18 +1,64 @@
 'use strict';
 
-(async function() {
-  try {
-    const result = await fetch('https://api.github.com/users/octocat');
+const API_KEY = '76ed63f80dba3e42bfe198c0806fa9ba';
+const IMG_SRC = 'https://image.tmdb.org/t/p/w500_and_h282_face/';
+const movieListContainer = document.querySelector('.movie-list');
+let currentPage = 1;
 
-    if (result.status === 403) throw new Error(result.statusText);
-    const data = await result.json();
+//https://api.themoviedb.org/3/movie/popular?api_key=76ed63f80dba3e42bfe198c0806fa9ba&language=en-US&page=1
 
-    const image = document.createElement('img');
-    image.src = data.avatar_url;
+const fetchMovieList = (page = 1) => {
+  currentPage = currentPage + 1;
+  return fetch(
+    `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}&language=en-US&page=${page}`,
+  ).then(result => result.json());
+};
 
-    document.body.appendChild(image);
-  } catch (error) {
-    console.log(111111111);
-    console.dir(error);
-  }
+const createMovieItem = ({ title, vote_average, vote_count, poster_path }) => {
+  return ` <div class="movie-item">
+              <img src="${IMG_SRC +
+                poster_path}" alt="${title}" class="movie-item__img" />
+              <h2 class="movie-item__title">${title}</h2>
+              <span class="movie-item__rating">Rating(${vote_count}): ${vote_average}</span>
+          </div>`;
+};
+
+const createMovieList = movieList => {
+  return movieList.reduce((acc, movie) => {
+    return acc + createMovieItem(movie);
+  }, '');
+};
+
+const getMovies = page => {
+  fetchMovieList(page)
+    .then(data => {
+      renderMovieList(data.results);
+    })
+    .catch(error => {
+      throw error;
+    });
+};
+
+const renderMovieList = movieList => {
+  movieListContainer.insertAdjacentHTML(
+    'beforeend',
+    createMovieList(movieList),
+  );
+};
+
+const addInfinityScroll = () => {
+  window.addEventListener('scroll', () => {
+    const movieListBottom = movieListContainer.getBoundingClientRect().bottom;
+    const windowHeight = window.innerHeight;
+
+    if (movieListBottom / 2 <= windowHeight) {
+      console.log('rich bottom');
+      getMovies(currentPage);
+    }
+  });
+};
+
+(function() {
+  getMovies();
+  addInfinityScroll();
 })();
